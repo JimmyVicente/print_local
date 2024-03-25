b36d12dbf4139051d3a619bba6cacfaf7d3f5877
JimmyVicente <46094347+JimmyVicente@users.noreply.github.com>
2024-03-25 15:48:36 -0500

    Update Size image

--- lib/print_local.dart
+++ lib/print_local.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-import 'dart:io';
 
+import 'dart:ui' as ui;
 import 'package:flutter/services.dart';
 import 'package:print_local/model.dart';
 
@@ -8,7 +8,6 @@ class PrintLocal {
   final methodChannel = const MethodChannel('print_local');
 
   Future<PrintResult> isPrintNative() async {
-    if (!Platform.isAndroid) PrintResult.notPlatform;
     var value = 5;
     try {
       var val = await methodChannel.invokeMethod<int?>('isPrintNative');
@@ -21,12 +20,16 @@ class PrintLocal {
 
   Future<ObjectPrintResult> printNative(Uint8List image) async {
     var message = "unknown error flutter";
-    if (!Platform.isAndroid) {
-      return ObjectPrintResult(PrintResult.notPlatform, message, -10);
-    }
     var valueR = ObjectPrintResult(PrintResult.unknownError, message, -10);
     try {
-      var imageBase64 = base64Encode(image);
+      var size = await getSizeImage(image);
+      size = getResizedSize(size, 380); //380 width
+      var imageAux = await renderSizeIcon(
+        image,
+        size.width.toInt(),
+        size.height.toInt(),
+      );
+      var imageBase64 = base64Encode(imageAux);
       final data = await methodChannel.invokeMethod('printNative', {
         'imageBase64': imageBase64,
       });
@@ -41,4 +44,30 @@ class PrintLocal {
     }
     return valueR;
   }
+
+  Future<Uint8List?> renderSizeIcon(Uint8List imageBytes,
+      int targetWidth,
+      int targetHeight) async {
+    final ui.Codec markerImageCodec = await ui.instantiateImageCodec(imageBytes,
+        targetWidth: targetWidth, targetHeight: targetHeight);
+    final ui.FrameInfo frameInfo = await markerImageCodec.getNextFrame();
+    final ByteData? byteData =
+    await frameInfo.image.toByteData(format: ui.ImageByteFormat.png);
+    final Uint8List? resizedMarkerImageBytes = byteData?.buffer.asUint8List();
+    return resizedMarkerImageBytes;
+  }
+
+  Size getResizedSize(Size originalSize, double targetWidth) {
+    double targetHeight =
+        (originalSize.height * targetWidth) / originalSize.width;
+    return Size(targetWidth, targetHeight);
+  }
+
+  Future<Size> getSizeImage(Uint8List imageData) async {
+    ui.Codec codec = await ui.instantiateImageCodec(imageData);
+    ui.FrameInfo f = await codec.getNextFrame();
+    Size size = Size(f.image.width.toDouble(), f.image.height.toDouble());
+    return size;
+  }
+
 }
